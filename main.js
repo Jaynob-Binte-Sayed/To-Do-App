88c3f1ef802f9fd6c6b2813cde1e0b06697c25c8
Jaynob <jynobpapiya12@gmail.com>
2023-03-21 23:38:50 +0600

    Accept and show the input value

---
+++ main.js
@@ -0,0 +1,64 @@
+let form = document.getElementById("form");
+let textInput = document.getElementById("textInput");
+let msg = document.getElementById("msg");
+let dateInput = document.getElementById("dateInput");
+let textarea = document.getElementById("textarea");
+let tasks = document.getElementById("tasks");
+let add = document.getElementById("add");
+
+form.addEventListener('submit', (e)=> {
+    e.preventDefault();
+    formValidation();
+
+})
+
+let formValidation = ()=> {
+    if(textInput.value === "") {
+        console.log('Failure');
+        msg.innerHTML = "Task cannot be blank";
+
+    }
+    else {
+        console.log('Success')
+        msg.innerHTML = "";
+        acceptData();
+        add.setAttribute("data-bs-dismiss" , "modal");
+        add.click();
+
+        
+
+    }
+};
+
+let data = {}
+
+let acceptData = ()=> {
+    data["text"] = textInput.value;
+    data["dateInput"] = dateInput.value;
+    data["description"] = textarea.value;
+    createTasks();
+}; 
+
+let createTasks = ()=> {
+    tasks.innerHTML += `
+    <div>
+        <span class="fw-bold">${data.text}</span>
+        <span class="small text-secondary">${data.date}</span>
+        <p>${data.description}</p>
+
+        <span class="options">
+            <i class="fa-solid fa-pen-to-square"></i>
+            <i class="fa-solid fa-trash"></i>
+        </span>
+    </div>`;
+
+    resetForm();
+
+};
+
+
+let resetForm = ()=> {
+    textInput.value = "";
+    dateInput.value = "";
+    textarea.value = "";
+}
